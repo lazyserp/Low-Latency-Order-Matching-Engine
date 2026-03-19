8133cdce20ee02e010676c224b3f73db2b4eaacf
Aman <amanjha434@gmail.com>
2026-03-19 22:49:11 +0530

    feat : Order Book to track the sequnece of orders and add them to their respective linked lists

---
+++ core/include/engine/OrderBook.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <map>
+#include "PriceLevel.hpp"
+
+class OrderBook
+{
+    public:
+        // Bids: Sorted high to low (std::greater)
+        std::map<double, PriceLevel, std::greater<double>> bids;
+
+        //// Asks: Sorted low to high (std::less)
+        std::map<double, PriceLevel, std::less<double>> asks;
+
+        void addOrder(Order* order)
+        {
+            if ( order->side == Side::Buy)
+            {
+                bids[order->price].price = order->price;
+                bids[order->price].addOrder(order);
+            }
+            else
+            {
+                asks[order->price].price = order->price;
+                asks[order->price].addOrder(order);
+            }
+        }
+
+
+}
