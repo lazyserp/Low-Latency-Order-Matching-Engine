808f9654c022ab0e1fdf86eb48d6a387611b29be
Aman <amanjha434@gmail.com>
2026-03-19 21:48:15 +0530

    feat : Added doubley Linked list to store the insertion seq of each order and group them

---
+++ core/include/engine/PriceLevel.hpp
@@ -0,0 +1,41 @@
+// A price level = all orders that have the SAME price
+#pragma once
+#include "common/Types.hpp"
+
+struct PriceLevel
+{
+    // the Orders are stored as double linked list: 1.to maintain the insertion order , 2. to delete a specific order
+    double price;
+    Order* head = nullptr;
+    Order* tail = nullptr;
+
+    void addOrder(Order* order)
+    {
+        if (!head)
+        {
+            head = tail = order;
+        }
+        else
+        {
+            tail->next = order;
+            order->prev = tail;
+            tail = order;
+        }
+    }
+
+    void removeOrder(Order* order)
+    {
+        if ( order->prev)
+        {
+            order->prev->next = order->next;
+        } 
+        if ( order->next)
+        {
+            order->next->prev = order->prev;
+        }
+        if( order == head) head = order->next;
+        if ( order == tail) tail = order->prev;
+
+        order->next = order->prev = nullptr;
+    }
+}
